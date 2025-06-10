6a0c4dd9de2ed7ff1e8bef325458fa3488df5da9
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-06-10 10:12:12 -0700

    Adding css, adding data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
 import CatList from './components/CatList';
+import DATA from './data.js';
 
 function App() {
   
   return (
     <>
-      <CatList></CatList>
+      <CatList catData={DATA}></CatList>
     </>
   );
 }
--- src/components/CatList.jsx
+++ src/components/CatList.jsx
@@ -1,10 +1,12 @@
 import Cat from './Cat.jsx';
 
-const CatList = () => {
+const CatList = (props) => {
     return (
         <>
           <h1>Cat List</h1>
             <ul>
+              <Cat ></Cat>
+              <Cat></Cat>
               <Cat></Cat>
               <Cat></Cat>
             </ul>
